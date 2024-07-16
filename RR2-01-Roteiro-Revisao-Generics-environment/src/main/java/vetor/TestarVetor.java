b0608b56c18309c54d73990303773653aba3398f
Neto <antoniobarros3583@gmail.com>
2024-07-15 22:25:17 -0300

    final do roteiro 2

--- RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/vetor/TestarVetor.java
+++ RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/vetor/TestarVetor.java
@@ -3,6 +3,7 @@ package vetor;
 public class TestarVetor {
 
 	public static void main(String[] args) {
+		// pode acrescentar testes a partir deste ponto
 		// preencha esse metodo com codigo para testar a classe vetor.
 		// À medida que voce evoluir no exercício o conteúdo deste metodo
 		// também será modificado.
