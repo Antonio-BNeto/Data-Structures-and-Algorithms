6b9f4ff32dfc87114f1eb8dae718722e284861eb
Antonio.Barros.Alcantara.Neto <128563411+Antonio-BNeto@users.noreply.github.com>
2024-07-19 10:32:36 -0300

    Add files via upload

---
+++ RR4-01-Roteiro-Revisao-Recursao-environment/src/main/java/recursao/TestarMetodosRecursivos.java
@@ -0,0 +1,10 @@
+package recursao;
+
+public class TestarMetodosRecursivos {
+	public static void main(String[] args) {
+		// preencha esse metodo com codigo para testar a classe MetodosRecursivos.
+		MetodosRecursivos metodos = new MetodosRecursivos();
+		System.out.println(metodos.calcularFatorial(5));
+		System.out.println(metodos.calcularFatorial(6));
+	}
+}
