a9b1255a4868b990cfd1476c6762ca102c93b041
Neto <antoniobarros3583@gmail.com>
2024-08-11 22:57:07 -0300

    inicio dos testes

--- R05-01-Rot-KLargestQuickSelectFloor-environment/src/test/java/problems/FloorBinarySearchTest.java
+++ R05-01-Rot-KLargestQuickSelectFloor-environment/src/test/java/problems/FloorBinarySearchTest.java
@@ -8,61 +8,43 @@ import org.junit.Test;
 import problems.FloorBinarySearchImpl;
 
 public class FloorBinarySearchTest {
-    private static final int Integer = 0;
-    private FloorBinarySearchImpl implementation;
-    private Integer[] array;
-    private Integer[] arrayVazio;
+    private FloorBinarySearchImpl floorBinarySearch;
+
+    private Integer[] vetorTamPar;
+	private Integer[] vetorTamImpar;
+	private Integer[] vetorVazio = {};
+	private Integer[] vetorValoresRepetidos;
+	private Integer[] vetorValoresIguais;
 
     @Before
     public void setUp(){
-        this.implementation = new FloorBinarySearchImpl();
-        this.array = new Integer[] { -3, 18, 4, 15, 11, 40, 45, 21, 13, 12 };
-        this.arrayVazio = new Integer[] {};
-    }
-
-    // Casos Válidos
-    @Test
-    public void testProblems00() {
-        assertEquals(this.implementation.floor(this.array, -3), (Integer) (-3));
+        this.floorBinarySearch = new FloorBinarySearchImpl();
+        this.vetorTamPar = new Integer[]{0, 1, 3, 4, 10, -1, 28, 13, 8, 20};
+        this.vetorTamImpar = new Integer[]{10, 23, 46, -54, 32, 10, 34, 12, 13, 22, 33};
+        this.vetorValoresIguais = new Integer[]{1, 2, 4, 5, 1, 68, -1, 2, 1};
+        this.vetorValoresRepetidos = new Integer[]{4, 4, 4, 4, 4, 4, 4, 4};
+        this.vetorVazio = new Integer[]{};
     }
 
     @Test
-    public void testProblems01() {
-        assertEquals(this.implementation.floor(this.array, 21), (Integer) 21);
+    public void test01(){
+        assertEquals((Integer)(-1), floorBinarySearch.floor(vetorTamPar, -1));
     }
 
     @Test
-    public void testProblems02() {
-        assertEquals(this.implementation.floor(this.array, 22), (Integer) 21);
+    public void test02(){
+        assertEquals((Integer) 0 , floorBinarySearch.floor(vetorTamPar, 0));
     }
 
     @Test
-    public void testProblems03() {
-        assertEquals(this.implementation.floor(this.array, 14), (Integer) 13);
-    }
-    // Casos Inválidos
-
-    // Quando o elemento passado é null
-    @Test
-    public void testProblems04() {
-        assertEquals(this.implementation.floor(this.array, null), null);
+    public void test03(){
+        assertEquals((Integer) 28, floorBinarySearch.floor(vetorTamPar, 30));
     }
 
-    // Quando o array passado é null
     @Test
-    public void testProblems05() {
-        assertEquals(this.implementation.floor(null, 2), null);
+    public void test04(){
+        assertEquals((Integer) 4,floorBinarySearch.floor(vetorTamPar, 5));
     }
 
-    // Quando o array é vazio
-    @Test
-    public void testProblems06() {
-        assertEquals(this.implementation.floor(this.arrayVazio, 2), null);
-    }
-
-    // Quando não tem um elemento menor que o passado no parâmetro, no array
-    @Test
-    public void testProblems07() {
-        assertEquals(this.implementation.floor(this.array, -4), null);
-    }
+    
 }
