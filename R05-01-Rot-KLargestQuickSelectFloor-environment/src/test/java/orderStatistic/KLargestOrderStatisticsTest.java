27cca2bab07ef7259a2670b6130d6d9c5ad88fff
Neto <antoniobarros3583@gmail.com>
2024-08-13 03:09:50 -0300

    Inicio dos testes do kLargest

--- R05-01-Rot-KLargestQuickSelectFloor-environment/src/test/java/orderStatistic/KLargestOrderStatisticsTest.java
+++ R05-01-Rot-KLargestQuickSelectFloor-environment/src/test/java/orderStatistic/KLargestOrderStatisticsTest.java
@@ -1,13 +1,105 @@
 package orderStatistic;
 
+import org.junit.Assert;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class KLargestOrderStatisticsTest {
-    public KLargestOrderStatisticsImpl<Integer> kLargest;
+
+    private Integer[] vetorTamPar;
+	private Integer[] vetorTamImpar;
+	private Integer[] vetorVazio = {};
+	private Integer[] vetorValoresRepetidos;
+	private Integer[] vetorValoresIguais;
+
+	public KLargest<Integer> implementation;
+
+	@Before
+	public void setUp() {
+		populaVetorTamanhoPar(new Integer[] { 30, 28, 7, 29, 11, 26, 4, 22, 23,
+				31 });
+		populaVetorTamanhoImpar(new Integer[] { 6, 41, 32, 7, 26, 4, 37, 49,
+				11, 18, 36 });
+		populaVetorRepetido(new Integer[] { 4, 9, 3, 4, 0, 5, 1, 4 });
+		populaVetorIgual(new Integer[] { 6, 6, 6, 6, 6, 6 });
+
+		getImplementation();
+	}
+
+	// // MÉTODOS AUXILIARES DA INICIALIZAÇÃO
+	/**
+	 * Método que inicializa a implementação a ser testada com a implementação
+	 * do aluno
+	 */
+	private void getImplementation() {
+		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
+		// null
+		this.implementation = new KLargestOrderStatisticsImpl<Integer>();
+		//Assert.fail("Implementation not provided");
+	}
+
+	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
+		this.vetorTamPar = Arrays.copyOf(arrayPadrao, arrayPadrao.length);
+	}
+
+	public void populaVetorTamanhoImpar(Integer[] arrayPadrao) {
+		this.vetorTamImpar = Arrays.copyOf(arrayPadrao, arrayPadrao.length);
+	}
+
+	public void populaVetorRepetido(Integer[] arrayPadrao) {
+		this.vetorValoresRepetidos = Arrays.copyOf(arrayPadrao,
+				arrayPadrao.length);
+	}
+
+	public void populaVetorIgual(Integer[] arrayPadrao) {
+		this.vetorValoresIguais = Arrays
+				.copyOf(arrayPadrao, arrayPadrao.length);
+	}
+
+    public void genericTest(Integer[] array, int k) {
+		Integer[] copy1 = {};
+        Integer[] copy2 = {};
+		if(array.length > 0){
+			copy1 = Arrays.copyOf(array, array.length);			
+		}
+		implementation.getKLargest(array, k);
+		Arrays.sort(copy1);
+        copy2 = Arrays.copyOfRange(copy1, array.length-k, array.length);
+		Assert.assertArrayEquals(copy2, array);
+	}
+
+    @Test
+	public void testSort01() {
+		genericTest(vetorTamPar, 3);
+	}
+
+	@Test
+	public void testSort02() {
+		genericTest(vetorTamImpar, 5);
+	}
+
+	@Test
+	public void testSort03() {
+		genericTest(vetorVazio, 0);
+	}
+
+	@Test
+	public void testSort04() {
+		genericTest(vetorValoresIguais, 0);
+	}
+
+	@Test
+	public void testSort05() {
+		genericTest(vetorValoresRepetidos, 0);
+	}
+
+
+   /*  public KLargest<Integer> kLargest;
     private Integer[] vetorTamPar;
 	private Integer[] vetorTamImpar;
 	private Integer[] vetorVazio = {};
@@ -32,5 +124,6 @@ public class KLargestOrderStatisticsTest {
         Integer[] result = new Integer[]{28, 20, 13};
        assertArrayEquals(result, KLargest.getKLargest(vetorTamPar, 3));
     }
+    */
 
 }
