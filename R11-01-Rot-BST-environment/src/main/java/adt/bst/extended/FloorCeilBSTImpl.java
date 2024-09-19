3d8b286ef3b3351ff2552cd8f7b31a4412141ce7
Neto <antoniobarros3583@gmail.com>
2024-09-18 23:56:54 -0300

    final do roteiro 11

--- R11-01-Rot-BST-environment/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
+++ R11-01-Rot-BST-environment/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
@@ -1,6 +1,7 @@
 package adt.bst.extended;
 
 import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
 
 /**
  * Note que esta classe estende sua implementacao de BST (BSTImpl).
@@ -11,14 +12,57 @@ public class FloorCeilBSTImpl extends BSTImpl<Integer> implements FloorCeilBST {
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		Integer floor = null;
+
+		if(array != null && array.length > 0 ){
+			for(Integer values: array){
+				this.insert(values);
+			}
+			floor = this.floor(this.root, numero, null);
+		}
+
+		return floor;
+	}
+
+	private Integer floor(BSTNode<Integer> node, double number, Integer floor){
+		if(!node.isEmpty()){
+			if(number < node.getData()){
+				floor = this.floor((BSTNode<Integer>) node.getLeft(), number, floor);
+			}else if(number > node.getData()){
+				floor = this.floor((BSTNode<Integer>) node.getRight(), number, node.getData());
+			}else{
+				floor = node.getData();
+			}
+		}
+		return floor;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		Integer ceil = null;
+
+		if(array != null && array.length > 0 ){
+			for(Integer values: array){
+				this.insert(values);
+			}
+			ceil = this.ceil(this.root, numero, null);
+		}
+
+		return ceil;
+	}
+
+	private Integer ceil(BSTNode<Integer> node, double number, Integer ceil){
+		if(!node.isEmpty()){
+			if(number < node.getData()){
+				ceil = this.floor((BSTNode<Integer>) node.getLeft(), number, node.getData());
+			}else if(number > node.getData()){
+				ceil = this.floor((BSTNode<Integer>) node.getRight(), number, ceil);
+			}else{
+				ceil = node.getData();
+			}
+		}
+		return ceil;
+
 	}
 
 }
